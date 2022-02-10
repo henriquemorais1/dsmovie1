d4c0b9be245614481261b1f48bf32523b1a04d8a
henriquemorais1 <99363989+henriquemorais1@users.noreply.github.com>
2022-02-10 15:52:32 -0300

    firts 1

---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+
+function Listing() {
+
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/movies?size=12page=0`).then(response => {
+        console.log(response.data);
+    });
+
+
+
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div>
+                    <div className="row">
+                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </>
+
+
+    );
+}
+
+export default Listing;
